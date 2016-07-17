b44c88603521cc769551a9d166c18c969740fc9c
sanjeevanand <maxsanjeevanand@gmail.com>
2016-07-17 20:21:51 +0530

    seeding data rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#=====================seeding==================================
+#items=Item.create([{uni_id:"nokia1100",name:"mobile",cname:"nokia"},{uni_id:"nokia1120",name:"mobile",cname:"android"}])
+#i=ItemTotal.create({total:90,item:items.first})
+#c=Customer.create([{customer_name:"sanjeev"},{customer_name:"pavan"},{customer_name:"sandeep"}])
