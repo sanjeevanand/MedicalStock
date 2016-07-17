14d6cf6f86372fc0daf85846ce5fd8811fd61263
sanjeevanand <maxsanjeevanand@gmail.com>
2016-07-17 23:47:25 +0530

    add some more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #=====================seeding==================================
+#I=Item.create({uni_id:"iphone",name:"mobile",cname:"s plus"})
 #items=Item.create([{uni_id:"nokia1100",name:"mobile",cname:"nokia"},{uni_id:"nokia1120",name:"mobile",cname:"android"}])
 #i=ItemTotal.create({total:90,item:items.first})
+#pentry=ProductEntry.create({no_of_box:10,qty_in_box:9,total_qty:90,mrp:200.2,selling_price:205.5,item_id:I.id})
 #c=Customer.create([{customer_name:"sanjeev"},{customer_name:"pavan"},{customer_name:"sandeep"}])
+# c=Customer.first
+#sell=Sell.create({customer_id:c.id})
+#ps=ProductSold.create({no_of_box:1,qty_in_box:5,total_qty:5,item_id:I.id,sell_id:sell.id})
